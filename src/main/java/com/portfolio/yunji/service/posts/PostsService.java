a248e2f9be9453cd5ee1e7a7b09105d177edb267
yunji <83765550+Yunji-Oh@users.noreply.github.com>
2021-06-20 22:27:53 +0900

    webservice

---
+++ src/main/java/com/portfolio/yunji/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.portfolio.yunji.service.posts;
+
+import com.portfolio.yunji.domain.posts.PostsRepository;
+import com.portfolio.yunji.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
